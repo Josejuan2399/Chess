ab8130010c089e6bbf93c7686ef2b16f3bd918fb
Jose Juan C <Josejuancor2399@gmail.com>
2018-03-26 08:53:14 -0400

    Create Chessboard Class

---
+++ Chess.py
@@ -0,0 +1,39 @@
+chess_map_from_alpha_to_index = {
+    "a" : 0,
+    "b" : 1,
+    "c" : 2,
+    "d" : 3,
+    "e" : 4,
+    "f" : 5,
+    "g" : 6,
+    "h" : 7
+}
+
+chess_map_from_index_to_alpha = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h"
+}
+
+
+class Chessboard():
+  
+  def __init__(self):
+    
+    self.board = []
+    self.occupiedSpaces = []
+    
+    for columns in range(8):
+      for rows in range(1,9):
+        self.board.append(chess_map_from_index_to_alpha[columns] + str(rows))
+    
+  def showBoard(self):
+    return self.board
+
+
+
